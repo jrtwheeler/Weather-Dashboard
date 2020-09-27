e3e48cc17d583d5c28da61aa597200d263e21005
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-09-27 11:22:35 -0400

    API call working with variable properties

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,28 +1,14 @@
 var city = $("#city-input");
-var city_list = $(".input-list")
+var city_list = $(".input-list");
 var searchButton = $("#search-button");
 var clearButton = $("#clear-button");
-var cityinput = '';
-var key = "9d93230f3ad2bc78a7973c5234d7ba2e"
+var key = "9d93230f3ad2bc78a7973c5234d7ba2e";
 
-searchButton.on("click", search_and_save);
-clearButton.on("click", clear);
-
-function search_and_save() {
-  var cityinput = city.val();
-  saveLocal("city", cityinput);
-  renderRows ();
-}
-
-function clear() {
-  var cityinput = "";
-  saveLocal("city", cityinput);
-  location.reload();
-}
-
-function renderRows () {
+function renderRows() {
   var city_list_group = $("<UL>");
-  var city_list_item = $("<LI>").addClass("list-group-item").text(getLocal("city"))
+  var city_list_item = $("<LI>")
+    .addClass("list-group-item")
+    .text(getLocal("city"));
   city_list.append(city_list_group);
   city_list_group.append(city_list_item);
 }
@@ -35,17 +21,34 @@ function getLocal(key) {
   return JSON.parse(localStorage.getItem(key)) || {};
 }
 
-var url = "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=" + key;
+function search_and_save() {
+  var cityinput = city.val();
+  saveLocal("city", cityinput);
+  renderRows();
 
-  $.ajax({
-    url: url,
-    method: "GET",
-  }).then(function (response) {
-    //Return the results in an object
-    var result = response;
-    console.log(result)
-  });
+  if (getLocal(key) != "") {
+    var url =
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      getLocal("city") +
+      "&appid=" +
+      key;
+    alert(url);
+    $.ajax({
+      url: url,
+      method: "GET",
+    }).then(function (response) {
+      //Return the results in an object
+      var result = response;
+      console.log(result);
+    });
+  }
+}
 
-  
+function clear() {
+  var cityinput = "";
+  saveLocal("city", cityinput);
+  location.reload();
+}
 
-  
+searchButton.on("click", search_and_save);
+clearButton.on("click", clear);
