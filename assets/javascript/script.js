9c4ad9052730d0abcc22e61b656d33b1f693ae65
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-09-27 10:56:39 -0400

    Add clear button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,12 @@
 var city = $("#city-input");
-var city_list = $(".input-group")
-var searchButton = $(".btn-primary");
+var city_list = $(".input-list")
+var searchButton = $("#search-button");
+var clearButton = $("#clear-button");
+var cityinput = '';
 var key = "9d93230f3ad2bc78a7973c5234d7ba2e"
 
 searchButton.on("click", search_and_save);
+clearButton.on("click", clear);
 
 function search_and_save() {
   var cityinput = city.val();
@@ -11,10 +14,17 @@ function search_and_save() {
   renderRows ();
 }
 
-function renderRows () {
-  var city_list_div = $("<p>").addClass("col-12 card mt-3 mb-3").text(getLocal("city"))
-  city_list.append(city_list_div);
+function clear() {
+  var cityinput = "";
+  saveLocal("city", cityinput);
+  location.reload();
+}
 
+function renderRows () {
+  var city_list_group = $("<UL>");
+  var city_list_item = $("<LI>").addClass("list-group-item").text(getLocal("city"))
+  city_list.append(city_list_group);
+  city_list_group.append(city_list_item);
 }
 
 function saveLocal(key, search) {
