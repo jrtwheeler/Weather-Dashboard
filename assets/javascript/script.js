3f0bf6069dd6812ba824e4b8e64188dc2217b992
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-09-27 09:58:38 -0400

    Add rows when a city is queried

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,24 @@
-var city = $(".form-control").val();
-var searchButton = $(".input-group-prepend");
+var city = $("#city-input");
+var city_list = $(".input-group")
+var searchButton = $(".btn-primary");
 var key = "9d93230f3ad2bc78a7973c5234d7ba2e"
 
 searchButton.on("click", search_and_save);
 
 function search_and_save() {
-    saveLocal("city", city);
-    alert(city)
+  var cityinput = city.val();
+  saveLocal("city", cityinput);
+  renderRows ();
+}
+
+function renderRows () {
+  var city_list_div = $("<p>").addClass("col-12 card mt-3 mb-3").text(getLocal("city"))
+  city_list.append(city_list_div);
+
 }
 
 function saveLocal(key, search) {
-  localStorage.setItem(key, JSON.stringify(search).trim());
+  localStorage.setItem(key, JSON.stringify(search));
 }
 
 function getLocal(key) {
@@ -27,3 +35,7 @@ var url = "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=" + k
     var result = response;
     console.log(result)
   });
+
+  
+
+  
